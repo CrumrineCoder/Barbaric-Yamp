ba03ca586da3ad24ccc8bd198a28ba5f829bf963
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-06-16 21:24:28 -0500

    Played a sound clip!

--- soundboard/src/main.cpp
+++ soundboard/src/main.cpp
@@ -7,14 +7,7 @@
 
 int main()
 {
-    MCIERROR err = mciSendString("open \"*.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
-
-    if (err != 0) {
-        char errorText[256];
-        mciGetErrorString(err, errorText, sizeof(errorText));
-        std::cerr << "Failed to open fitness.mp3: " << errorText << std::endl;
-        MessageBoxA(NULL, errorText, "MCI Open Error", MB_OK | MB_ICONERROR);
-        return 1;
-    }
-    mciSendString("play mp3", NULL, 0, NULL);
+	std::cout << "Playing music \n";
+	PlaySound(TEXT("dayo.wav"), NULL, SND_FILENAME | SND_SYNC);
+	std::cout << "Music finished playing \n";
 }
