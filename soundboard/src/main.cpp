2ab38d974a81d9943d1b5a318072bfa58f5b3bc4
Nicolas Crumrine <crumrinecoding@gmail.com>
2025-06-16 21:13:20 -0500

    Trying to figure out how to play a file

--- soundboard/src/main.cpp
+++ soundboard/src/main.cpp
@@ -1,7 +1,20 @@
+#include <Windows.h>
+#include <Mmsystem.h>
+#include <mciapi.h>
 #include <iostream>
+
+#pragma comment(lib, "Winmm.lib")
 
 int main()
 {
-	std::cout << "Hello, world!";
-	return 0;
+    MCIERROR err = mciSendString("open \"*.mp3\" type mpegvideo alias mp3", NULL, 0, NULL);
+
+    if (err != 0) {
+        char errorText[256];
+        mciGetErrorString(err, errorText, sizeof(errorText));
+        std::cerr << "Failed to open fitness.mp3: " << errorText << std::endl;
+        MessageBoxA(NULL, errorText, "MCI Open Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+    mciSendString("play mp3", NULL, 0, NULL);
 }
